cc5de31a46442cf40fb02c6c85cd9dbc2ddc606a
Adam Harries <adam.harries@codeplay.com>
2019-07-23 16:08:54 +0100

    Move logic out of application, and into library

--- src/bin.rs
+++ src/bin.rs
@@ -1,121 +1,31 @@
 extern crate tizol;
+use tizol::Spectrogram;
 
-extern crate stft;
-use stft::{WindowType, STFT};
 
-extern crate image;
-use image::{ImageBuffer, RgbImage};
+use std::time::Instant;
 
-extern crate hodges;
-use hodges::*;
 
-extern crate scarlet;
-use crate::scarlet::colormap::ColorMap;
-
-use std::time::{Duration, Instant};
 
 fn main() {
     let start = Instant::now();
-    println!("==== Time: {:?}", start.elapsed());
-    println!("Reading file");
+
+    let log_time = |message: &'static str| -> () {
+        println!("==== Time: {: >15?} ==== {}", start.elapsed(), message);
+    };
+
+    log_time("Read file");
 
     let audio_f = "One O'Clock Jump - Metronome All Star Band.mp3";
 
-    let state: State<f32> = State::from_file(audio_f).expect("Failed to open file with libhodges");
+    let sp = Spectrogram::from_file(audio_f).unwrap();
 
-    let audio_samples: Vec<f64> = state.map(|f| f as f64).collect();
+    log_time("Computed spectrogram");
 
-    // Dump the audio samples
+    let img = sp.as_image();
 
-    println!("==== Time: {:?}", start.elapsed());
-    println!("Collected samples!");
-    println!("Found {} samples", audio_samples.len());
-
-    // let's initialize our short-time fourier transform
-    let window_type: WindowType = WindowType::Hanning;
-    let window_size: usize = 2048;
-    let step_size: usize = window_size / 4;
-    let mut stft = STFT::<f64>::new(window_type, window_size, step_size);
-
-    // we need a buffer to hold a computed column of the spectrogram
-    let mut spectrogram_column: Vec<f64> = std::iter::repeat(0.).take(stft.output_size()).collect();
-
-    let mut raw_spectrogram_data: Vec<f64> =
-        Vec::with_capacity(stft.output_size() * audio_samples.len() / window_size);
-
-    let low_freq = 0;
-    let hi_freq = 1024;
-
-    // iterate over all the samples in chunks of 3000 samples.
-    // in a real program you would probably read from something instead.
-    for some_samples in (&audio_samples[..]).chunks(4096) {
-        // append the samples to the internal ringbuffer of the stft
-        stft.append_samples(some_samples);
-
-        // as long as there remain window_size samples in the internal
-        // ringbuffer of the stft
-        while stft.contains_enough_to_compute() {
-            stft.compute_magnitude_column(&mut spectrogram_column[..]);
-            raw_spectrogram_data.extend(&spectrogram_column[low_freq..hi_freq]);
-            stft.move_to_next_column();
-        }
-    }
-
-    // let raw_spectrogram_data : Vec<f64> = tizol::util::read_sample_file("python_samples.txt");
-
-    tizol::amplitude_to_db(&mut raw_spectrogram_data[..]);
-    println!("==== Time: {:?}", start.elapsed());
-    println!(
-        "{} elements in raw_spectrogram_data",
-        raw_spectrogram_data.len()
-    );
-    println!(
-        "{} columns in raw_spectrogram_data",
-        raw_spectrogram_data.len() / stft.output_size()
-    );
-
-    // normalise the vector
-    let (min, max): (f64, f64) = raw_spectrogram_data.iter().map(|v| v .abs() ).fold(
-        (std::f64::MAX, std::f64::MIN),
-        |(mi, ma), x| {
-            let new_mi = if x < mi { x } else { mi };
-            let new_ma = if x > ma { x } else { ma };
-            (new_mi, new_ma)
-        },
-    );
-
-    let range = max - min;
-
-    println!("Range {} min {} max {}", range, min, max);
-    let normalized: Vec<f64> = raw_spectrogram_data
-        .iter()
-        .map(|v| 1.0 - ((v.abs() - min) / range))
-        .collect();
-
-    println!("==== Time: {:?}", start.elapsed());
-    println!("Normalized spectrogram data");
-
-    let image_height: usize = hi_freq - low_freq; //stft.output_size() as usize;
-    let image_width: usize = 1 + (raw_spectrogram_data.len() / image_height);
-
-    let mut img: RgbImage = ImageBuffer::new(image_width as u32, image_height as u32);
-
-    let colourmap = scarlet::colormap::ListedColorMap::magma();
-
-    let calculated_colours : Vec<scarlet::color::RGBColor>  = colourmap.transform(normalized);
-
-    println!("==== Time: {:?}", start.elapsed());
-    println!("Calculated colours");
-
-    for (c, column) in calculated_colours[..].chunks(image_height).enumerate() {
-        for (r, colour) in column.iter().enumerate() {
-            // let colour: scarlet::color::RGBColor = colourmap.transform_single(1.0 - spect_dat);
-            let p = image::Rgb([colour.int_r(), colour.int_g(), colour.int_b()]);
-            img.put_pixel(c as u32, image_height as u32 - r as u32 - 1, p);
-        }
-    }
+    log_time("Image generated!");
 
     img.save("test.png").unwrap();
-    println!("==== Time: {:?}", start.elapsed());
-    println!("Finished.");
+
+    log_time("Finished.");
 }
--- src/lib.rs
+++ src/lib.rs
@@ -1,99 +1,178 @@
-// extern crate itertools;
-// extern crate itertools_num;
+extern crate stft;
+use stft::{WindowType, STFT};
 
-// use itertools_num::linspace;
-// use itertools_num::Linspace;
+extern crate image;
+use image::{ImageBuffer, RgbImage};
 
-pub fn generate_random_samples(sample_rate: usize, seconds: usize) -> Vec<f64> {
-    let sample_count = sample_rate * seconds;
-    (0..sample_count).map(|x| x as f64).collect::<Vec<f64>>()
+extern crate hodges;
+use hodges::*;
+
+extern crate scarlet;
+use crate::scarlet::colormap::ColorMap;
+
+use std::time::Instant;
+
+use rayon::prelude::*;
+
+use std::fs;
+
+use std::path::{Path, PathBuf};
+
+pub struct Spectrogram {
+    pub data: Vec<f64>,
+    pub width: usize,
+    pub height: usize,
 }
 
-pub fn amplitude_to_db(S: &mut [f64]) -> () {
-    let amin: f64 = 1e-5;
-    let top_db: f64 = 80.0;
+impl Spectrogram {
+    /* Open an audio file, read it into a buffer, then call `from_buffer` */
+    pub fn from_file<P: Into<PathBuf>>(filename: P) -> Option<Self> {
+        // Create a hodges state object to load the audio data
+        let state: State<f32> = match State::from_file(filename) {
+            Some(s) => s,
+            None => {
+                println!("Failed to open file with libhodges");
+                return None;
+            }
+        };
 
-    let ref_value: f64 = S
-        .iter()
-        .map(|x| x.abs())
-        .fold(std::f64::MIN, |m, x| if x > m { x } else { m });
+        // Collect the audio samples into a single buffer for processing.
+        let audio_samples: Vec<f64> = state.map(|f| f as f64).collect();
 
-    // Don't forget to elementwise square S first!
-    for v in S.iter_mut() {
-        // Calculate magnitude
-        *v = v.abs();
-        // Calculate power
-        *v = *v * *v;
-    }
-    power_to_db(S, ref_value.powf(2.0), amin.powf(2.0), top_db);
-}
-
-pub fn power_to_db(S: &mut [f64], ref_value: f64, amin: f64, top_db: f64) -> () {
-    if amin <= 0.0 {
-        panic!("amin must be >= 0");
-    }
-    if top_db < 0.0 {
-        panic!("top_db must be > 0");
+        Self::from_buffer(&audio_samples)
     }
 
-    let ref_value = ref_value.abs();
+    pub fn from_buffer(audio_samples: &Vec<f64>) -> Option<Self> {
+        // Initialise the stft machinery.
+        let mut stft = STFT::<f64>::new(
+            WindowType::Hanning, /*The STFT window type*/
+            2048,                /* Window size */
+            2048 / 4,            /* Step size */
+        );
 
-    for v in S.iter_mut() {
-        // Calculate log_spec
-        *v = 10.0 * amin.max(*v).log10();
-        *v -= 10.0 * amin.max(ref_value).log10();
+        /* Create buffers for:
+            - The output of a single STFT column,
+            - The total output of the STFT
+        */
+        let mut spectrogram_column: Vec<f64> =
+            std::iter::repeat(0.).take(stft.output_size()).collect();
+
+        let mut spectrogram_output: Vec<f64> = Vec::with_capacity(audio_samples.len() * 2);
+
+        // Adjustable - for now, use the whole buffer.
+        let low_freq = 0;
+        let hi_freq = 1024;
+
+        // Perform the STFT across the samples
+        for samples in (&audio_samples[..]).chunks(4096) {
+            stft.append_samples(samples);
+            while stft.contains_enough_to_compute() {
+                stft.compute_magnitude_column(&mut spectrogram_column[..]);
+                spectrogram_output.extend(&spectrogram_column[low_freq..hi_freq]);
+                stft.move_to_next_column();
+            }
+        }
+
+        // Compute the amplitude_to_db of the result.
+        Self::amplitude_to_db(&mut spectrogram_output[..]);
+
+        // Normalize the output.
+        Self::normalize_buffer_inplace(&mut spectrogram_output[..]);
+
+        // Finally, calculate the height/width of the data.
+        let height = hi_freq - low_freq;
+        let width = (spectrogram_output.len() / height); // Guaranteed to be divisible
+
+        Some(Spectrogram {
+            data: spectrogram_output,
+            width: width,
+            height: height,
+        })
     }
 
-    let max: f64 = S
-        .iter()
-        .fold(std::f64::MIN, |m, x| if *x > m { *x } else { m });
+    fn normalize_buffer_inplace(buffer: &mut [f64]) -> () {
+        let (min, max): (f64, f64) =
+            buffer
+                .iter()
+                .map(|v| v.abs())
+                .fold((std::f64::MAX, std::f64::MIN), |(mi, ma), x| {
+                    let new_mi = if x < mi { x } else { mi };
+                    let new_ma = if x > ma { x } else { ma };
+                    (new_mi, new_ma)
+                });
 
-    // Second loop after we have the max
-    for v in S.iter_mut() {
-        *v = v.max(max - top_db);
+        let range = max - min;
+        for v in buffer.iter_mut() {
+            *v = 1.0 - ((v.abs() - min) / range);
+        }
     }
-}
 
-pub mod util {
-    use std::fs;
-    use std::path::Path;
-    pub fn dump_samples(samples: &Vec<f64>) -> String {
-        samples
+    fn amplitude_to_db(s: &mut [f64]) -> () {
+        let amin: f64 = 1e-5;
+        let top_db: f64 = 80.0;
+
+        let ref_value: f64 =
+            s.iter()
+                .map(|x| x.abs())
+                .fold(std::f64::MIN, |m, x| if x > m { x } else { m });
+
+        // Don't forget to elementwise square S first!
+        for v in s.iter_mut() {
+            // Calculate magnitude
+            *v = v.abs();
+            // Calculate power
+            *v = *v * *v;
+        }
+        Self::power_to_db(s, ref_value.powf(2.0), amin.powf(2.0), top_db);
+    }
+
+    fn power_to_db(s: &mut [f64], ref_value: f64, amin: f64, top_db: f64) -> () {
+        if amin <= 0.0 {
+            panic!("amin must be >= 0");
+        }
+        if top_db < 0.0 {
+            panic!("top_db must be > 0");
+        }
+
+        let ref_value = ref_value.abs();
+
+        for v in s.iter_mut() {
+            // Calculate log_spec
+            *v = 10.0 * amin.max(*v).log10();
+            *v -= 10.0 * amin.max(ref_value).log10();
+        }
+
+        let max: f64 = s
             .iter()
-            .map(|f| f.to_string())
-            .collect::<Vec<String>>()
-            .join(",")
+            .fold(std::f64::MIN, |m, x| if *x > m { *x } else { m });
+
+        // Second loop after we have the max
+        for v in s.iter_mut() {
+            *v = v.max(max - top_db);
+        }
     }
 
-    pub fn parse_samples(samples: String) -> Vec<f64> {
-        samples
-            .split(",")
-            .map(|f| f.parse::<f64>())
-            .collect::<Result<Vec<f64>, std::num::ParseFloatError>>()
-            .expect("Encountered an error while parsing floating point samples")
-    }
+    pub fn as_image(&self) -> RgbImage {
+        let mut img: RgbImage = ImageBuffer::new(self.width as u32, self.height as u32);
 
-    pub fn read_sample_file<P: AsRef<Path>>(filename: P) -> Vec<f64> {
-        parse_samples(fs::read_to_string(filename).expect("Failed to read string from file"))
-    }
+        let colourmap = scarlet::colormap::ListedColorMap::magma();
 
-    pub fn write_sample_file<P: AsRef<Path>>(filename: P, samples: &Vec<f64>) -> () {
-        fs::write(filename, dump_samples(samples)).expect("Unwable to write samples to a file");
+        let calculated_colours: Vec<scarlet::color::RGBColor> = self
+            .data
+            .par_iter()
+            .map(|n| colourmap.transform_single(*n))
+            .collect();
+
+        calculated_colours[..]
+            .chunks(self.height)
+            .enumerate()
+            .for_each(|(c, column)| {
+                column.iter().enumerate().for_each(|(r, colour)| {
+                    let p = image::Rgb([colour.int_r(), colour.int_g(), colour.int_b()]);
+                    img.put_pixel(c as u32, self.height as u32 - r as u32 - 1, p);
+                })
+            });
+
+        img
     }
 }
-
-// default: sr=22050, n_fft=2048
-// pub fn fft_frequencies(sr: u32, n_fft: u32) -> Linspace<f64> {
-//     linspace::<f64>(0.0, sr as f64 / 2.0, (1 + n_fft / 2) as usize)
-// }
-
-// def __coord_fft_hz(n, sr=22050, **_kwargs):
-//     '''Get the frequencies for FFT bins'''
-//     n_fft = 2 * (n - 1)
-//     # The following code centers the FFT bins at their frequencies
-//     # and clips to the non-negative frequency range [0, nyquist]
-//     basis = core.fft_frequencies(sr=sr, n_fft=n_fft)
-//     fmax = basis[-1]
-//     basis -= 0.5 * (basis[1] - basis[0])
-//     basis = np.append(np.maximum(0, basis), [fmax])
-//     return basis
